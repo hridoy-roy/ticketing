63ba4d714153c34c3a9adbbb63111d84b0884c8a
Md Shorov Abedin <shorov1952@gmail.com>
2022-10-29 10:42:21 +0600

    user-profile-update

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -13,9 +13,9 @@
     text-align: center;
   }
   .profile-left-tab li .active {
-    background-color: #e10000 !important;
-    color: #fff !important;
-  }
+  background-color: #03a050 !important;
+  color: #fff !important;
+}
   .profile-left-tab li a {
     padding: 8px 15px;
     display: block;
@@ -57,6 +57,53 @@
     display: flex;
     flex-direction: column;
 }
+.profile-right-tab-content .form-gp input {
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 10px;
+}
+.form-gp {
+  position: relative;
+  margin-bottom: 15px;
+}
+.form-gp label{
+    display:block;
+}
+.form-gp input {
+  width: 100%;
+  height: 44px;
+  border: 1px solid #c8c8c8;
+  outline: none !important;
+  box-shadow: none !important;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 14px;
+}
+.btn.mid_cont_btn {
+  color: #fff !important;
+  background: #03a050 ;
+  border-color: #03a050 ;
+  box-shadow: none !important;
+  font-size: 18px;
+  font-weight: 600;
+  outline: none !important;
+  transition: all ease-in-out .3s;
+  margin: 0 auto;
+  padding: 6px 50px;
+  border-radius: 30px;
+  min-height: 44px;
+  text-transform: uppercase;
+}
+.profile-right-tab-content .form-gp .icon {
+  top: 46px !important;
+}
+.form-gp .icon {
+  position: absolute;
+  right: 10px;
+  color: #3333336b;
+  font-size: 16px;
+  top: 13px;
+}
 </style>
 @section('main')
 <section class="login_section pt-t pt-md-5">
@@ -64,7 +111,7 @@
     <div class="row">
       <div class="col-md-3 col-sm-3 col-12">
         <ul class="nav nav-tabs profile-left-tab">
-          <li class="top-li my-3">
+          <li class="top-li my-2">
             <h4 class="name">Hammad Rahman</h4>
             <p class="user-phn">+880 1813871392</p>
           </li>
@@ -85,7 +132,7 @@
               <i class="fa fa-key"></i>&nbsp; Change password </a>
           </li>
           <li>
-            <a href="https://lalsobujbus.com/sign-out" class="text-dark">
+            <a href="#" class="text-dark">
               <i class="fa fa-sign-out-alt"></i>&nbsp; Sign out </a>
           </li>
         </ul>
@@ -135,22 +182,22 @@
                   <div class="form-gp">
                     <label for="cFirstName">First name</label>
                     <input id="cFirstName" type="text" value="Hammad" name="cFirstName" autocomplete="off" required="">
-                    <i class="fa fa-user"></i>
+                    <span class="icon"><i class="fa fa-user"></i></span>
                   </div>
                   <div class="form-gp">
                     <label for="cLastName">Last name</label>
                     <input id="cLastName" type="text" value="Rahman" name="cLastName" autocomplete="off" required="">
-                    <i class="fa fa-user"></i>
+                    <span class="icon"> <i class="fa fa-user"></i></span>
                   </div>
                   <div class="form-gp">
                     <label for="cusPhone">Enter Email</label>
                     <input id="cusPhone" type="text" disabled="disabled" value="+880 1813871392" name="phone" autocomplete="off" required="">
-                    <i class="fa fa-phone"></i>
+                    <span class="icon"> <i class="fa fa-phone"></i></span>
                   </div>
                   <div class="form-gp">
                     <label for="cusEmail">Email</label>
                     <input id="cusEmail" type="email" value="ikram.codeware@gmail.com" name="email" autocomplete="off">
-                    <i class="fa fa-envelope"></i>
+                    <span class="icon"> <i class="fa fa-envelope"></i></span>
                   </div>
                   <div class="submit-btn-area">
                     <button onclick="updateCustomer()" id="edit_form_submit" class="btn mid_cont_btn" type="button">Update profile</button>
@@ -169,19 +216,19 @@
                   <div class="form-gp togglePass1">
                     <label for="current_password">Current password</label>
                     <input id="current_password" type="password" name="current_password" autocomplete="off" required="">
-                    <i class="fa fa-eye eye_icon" id="currPassShowHide" onclick="showHide('one');"></i>
+                    <span class="icon">  <i class="fa fa-eye eye_icon" id="currPassShowHide" onclick="showHide('one');"></i></span>
                     <span class="errorTextMsg" id="currPassError"></span>
                   </div>
                   <div class="form-gp togglePass2">
                     <label for="new_password">New password</label>
                     <input id="new_password" type="password" name="new_password" autocomplete="off" required="">
-                    <i class="fa fa-eye eye_icon" id="newPasswordShowHide" onclick="showHide('two');"></i>
+                    <span class="icon">   <i class="fa fa-eye eye_icon" id="newPasswordShowHide" onclick="showHide('two');"></i></span>
                     <span class="errorTextMsg" id="newPassError"></span>
                   </div>
                   <div class="form-gp togglePass3">
                     <label for="confirm_password">Confirm Password</label>
                     <input id="confirm_password" type="password" name="confirm_password" autocomplete="off" required="">
-                    <i class="fa fa-eye eye_icon" id="confirmPasswordShowHide" onclick="showHide('three');"></i>
+                    <span class="icon">  <i class="fa fa-eye eye_icon" id="confirmPasswordShowHide" onclick="showHide('three');"></i></span>
                     <span class="errorTextMsg" id="confPassError"></span>
                   </div>
                   <div class="submit-btn-area">
